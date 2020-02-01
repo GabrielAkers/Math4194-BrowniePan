da66148e4a7e9b657161ddf7498dbee1ba3cb02a
GabrielAkers <gabe.akers17@gmail.com>
2020-02-01 03:20:18 -0500

    radius slider and text display and also new shape

--- main_sim.py
+++ main_sim.py
@@ -9,6 +9,10 @@ SIZE = [500, 500]
 SCREEN = pygame.display.set_mode(SIZE)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+GREY = (200, 200, 200)
+TRANS = (1, 1, 1)
+ORANGE = (200, 100, 50)
+
 
 # some predefined polygons for easy testing
 EQ_TRI = [(3/4*SIZE[0], 1/4*SIZE[1]),
@@ -41,16 +45,17 @@ class Shape:
         self.radius = radius  # this is for rounded rect or 'rr' flagged shapes, 0<=r<=1 : 0=>square and 1=>circle
 
         self.shape_type = shape_type
+        self.shape = None
         self.create_self()
 
         self.area = self.calc_area()
 
     def create_self(self):
         if self.shape_type == 'poly':
-            pygame.draw.polygon(SCREEN, WHITE, self.vertices)
+            self.shape = pygame.draw.polygon(SCREEN, WHITE, self.vertices)
         elif self.shape_type == 'rr':
             r = pygame.Rect(SQ[0][0], SQ[0][1], 250, 250)
-            self.aa_filled_rounded_rect(SCREEN, r, WHITE, self.radius)
+            self.shape = self.aa_filled_rounded_rect(SCREEN, r, WHITE, self.radius)
 
     def calc_area(self):
         # arbitrary polygon area algorithm found here: https://www.mathopenref.com/coordpolygonarea2.html
@@ -100,21 +105,81 @@ class Shape:
         rectangle.fill(color, special_flags=pygame.BLEND_RGBA_MAX)
         rectangle.fill((255, 255, 255, alpha), special_flags=pygame.BLEND_RGBA_MIN)
 
-        surface.blit(rectangle, pos)
+        return surface.blit(rectangle, pos)
+
+
+# slider stuff borrowed from here: https://www.dreamincode.net/forums/topic/401541-buttons-and-sliders-in-pygame/
+font = pygame.font.SysFont("Verdana", 12)
+
+
+class Slider:
+    def __init__(self, name, val, maxi, mini, pos):
+        self.val = val  # start value
+        self.maxi = maxi  # maximum at slider position right
+        self.mini = mini  # minimum at slider position left
+        self.xpos = pos  # x-location on screen
+        self.ypos = 450
+        self.surf = pygame.Surface((100, 50))
+        self.hit = False  # the hit attribute indicates slider movement due to mouse interaction
+
+        self.txt_surf = font.render(name, 1, WHITE)
+        self.txt_rect = self.txt_surf.get_rect(center=(50, 15))
+
+        # Static graphics - slider background #
+        self.surf.fill((100, 100, 100))
+        pygame.draw.rect(self.surf, GREY, [0, 0, 100, 50], 3)
+        pygame.draw.rect(self.surf, ORANGE, [10, 10, 80, 10], 0)
+        pygame.draw.rect(self.surf, WHITE, [10, 30, 80, 5], 0)
+
+        self.surf.blit(self.txt_surf, self.txt_rect)  # this surface never changes
+
+        # dynamic graphics - button surface #
+        self.button_surf = pygame.Surface((20, 20))
+        self.button_surf.fill(TRANS)
+        self.button_surf.set_colorkey(TRANS)
+        pygame.draw.circle(self.button_surf, WHITE, (10, 10), 6, 0)
+        pygame.draw.circle(self.button_surf, ORANGE, (10, 10), 4, 0)
+
+    def draw(self):
+        """ Combination of static and dynamic graphics in a copy of
+    the basic slide surface
+    """
+        # static
+        surf = self.surf.copy()
+
+        # dynamic
+        pos = (10+int((self.val-self.mini)/(self.maxi-self.mini)*80), 33)
+        self.button_rect = self.button_surf.get_rect(center=pos)
+        surf.blit(self.button_surf, self.button_rect)
+        self.button_rect.move_ip(self.xpos, self.ypos)  # move of button box to correct screen position
+
+        # screen
+        SCREEN.blit(surf, (self.xpos, self.ypos))
+
+    def move(self):
+        """
+    The dynamic part; reacts to movement of the slider button.
+    """
+        self.val = (pygame.mouse.get_pos()[0] - self.xpos - 10) / 80 * (self.maxi - self.mini) + self.mini
+        if self.val < self.mini:
+            self.val = self.mini
+        if self.val > self.maxi:
+            self.val = self.maxi
 
 
 class Sim:
     def __init__(self):
         self.play = True
         self.shape = None
+        self.radius_slider = Slider("Radius", 0.6, 1.0, 0.0, 0)
+        self.slides = [self.radius_slider]
 
-    def new_sim(self, shape_type='rr', radius=0.7):
+    def new_sim(self, shape_type='rr'):
         SCREEN.fill(BLACK)
-        self.shape = Shape(SQ, shape_type=shape_type, radius=radius)
+        self.shape = Shape(SQ, shape_type=shape_type, radius=self.radius_slider.val)
 
     def run(self):
         while self.play:
-            pygame.display.update()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -123,6 +188,32 @@ class Sim:
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_n:
                         self.new_sim()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    pos = pygame.mouse.get_pos()
+                    for s in self.slides:
+                        if s.button_rect.collidepoint(pos):
+                            s.hit = True
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    for s in self.slides:
+                        s.hit = False
+
+            for s in self.slides:
+                if s.hit:
+                    s.move()
+
+            for s in self.slides:
+                s.draw()
+
+            # just display the value of the radius
+            txt_surf = font.render(str(self.radius_slider.val), 1, WHITE)
+            txt_rect = txt_surf.get_rect(center=(50, 15))
+            SCREEN.fill(BLACK, pygame.Rect(0, 0, 100, 30))
+            SCREEN.blit(txt_surf, txt_rect)
+
+            if self.shape:
+                print(self.shape.shape)
+
+            pygame.display.update()
 
 
 os.chdir(os.path.dirname(__file__))
